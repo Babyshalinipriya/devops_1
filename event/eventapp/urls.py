c7f5117179553c59e62b7fea686df25beaf05f49
Babyshalinipriya <129202588+Babyshalinipriya@users.noreply.github.com>
2023-11-01 12:42:07 +0530

    Delete event directory

--- event/eventapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .import views
-urlpatterns=[
-    path("",views.htmlexample,name="htmlexample"),
-    path("signin/",views.signin,name="signin"),
-    path("main/",views.main,name="main")
-]
